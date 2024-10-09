8765ef1caff618d7f35ca8abca2843ffbfbf67cf
Chikage Molander <chikage.t.molander@gmail.com>
2024-10-09 16:15:14 +0200

    Setup NativeWind and implement initial screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,42 @@
-import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+import { View, Text, ImageBackground, SafeAreaView } from 'react-native'
+import React from 'react'
+import { LinearGradient } from 'expo-linear-gradient'
+import { StatusBar } from 'expo-status-bar'
+ 
+import beachImage from "@/assets/meditation-images/beach.webp";
+import CustomButton from '@/components/CustomButton';
 
-
-export default function HomeScreen() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
+    <View className='flex-1'>
+      <ImageBackground
+        source={beachImage}
+        resizeMode='cover'
+        className='flex-1'
+      >
+        <LinearGradient className='flex-1' colors={["rgba(0, 0, 0, 0.4)", "rgba(0, 0, 0, 0.8)"]}>
+          <SafeAreaView className='flex-1 mx-5 my-12 justify-between'>
+            <View>
+              <Text className='text-center text-white font-bold text-4xl'>
+                Simple Meditation
+              </Text>
+
+              <Text className='text-center text-white font-regular text-2xl mt-3'>
+                Simplifying Meditaion for Everyone
+              </Text>
+            </View>
+
+            <View>
+              <CustomButton onPress={() => console.log('tap')} title='Get Started' />
+            </View>
+
+            <StatusBar style='light' />
+          </SafeAreaView>
+        </LinearGradient>
+
+      </ImageBackground>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    flex: 1,
-    justifyContent: "center",
-  }
-});
+export default App
